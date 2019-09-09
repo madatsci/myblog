89c83882f2d7dd37da0f1839270ad922d5923dcc
Andrey Shevkalyuk <m-dec@yandex.ru>
2019-09-09 10:11:58 +0300

    Update Config.php

--- app/core/Config.php
+++ app/core/Config.php
@@ -11,11 +11,11 @@ class Config
     public $baseUrl = 'http://myblog.local/';
     public $tplDir = '../app/views/';
 
-    public $dbHost = 'localhost';
-    public $dbUser = 'root';
+    public $dbHost = '';
+    public $dbUser = '';
     public $dbPassword = '';
-    public $dbName = 'myblog';
-    public $dbPrefix = 'blog_';
+    public $dbName = '';
+    public $dbPrefix = '';
     
     public $minLoginLength = 3;
     public $maxLoginLength = 255;
